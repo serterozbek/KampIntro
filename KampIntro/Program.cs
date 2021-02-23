649c54b582e62fdbe9893bcc9df38a8b1200c237
serterozbek <serter.ozbek@hotmail.com>
2021-02-23 21:11:10 +0300

    Add project files.

---
+++ KampIntro/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace KampIntro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //type safety = tip güvenliği
+            //Do not repeat yourself
+            //değer tutucu
+            string kategoriEtiketi = "Kategoriler";
+            int ogrenciSayisi = 32000;
+            double faizOrani = 1.45;
+            bool sistemeGirisYapmisMi = false;
+            double dolarDun = 7.35;
+            double dolarBugun = 7.45;
+
+            if (dolarDun>dolarBugun)
+            {
+                Console.WriteLine("Azalis butonu");
+            }
+            else if (dolarDun<dolarBugun)
+            {
+                Console.WriteLine("Artis butonu");
+            }
+            else
+            {
+                Console.WriteLine("degismedi butonu");
+            }
+
+            if (sistemeGirisYapmisMi == true)
+            {
+                Console.WriteLine("Kullanıcı ayarları butonu");
+            }
+            else
+            {
+                Console.WriteLine("Giris yap butonu");
+            }
+            Console.WriteLine(kategoriEtiketi);
+            Console.WriteLine(kategoriEtiketi);
+            Console.WriteLine(kategoriEtiketi);
+            Console.WriteLine(kategoriEtiketi);
+            Console.WriteLine(kategoriEtiketi);
+            Console.WriteLine(kategoriEtiketi);
+        }
+    }
+}
---
+++ Metotlar/SepetManager.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Metotlar
+{
+    class SepetManager
+    {
+        //naming convention ---> E büyük yazılır.
+        //syntax
+        public void Ekle(Urun urun) //Urun urun -- tipi, vereceğim isimlendirme
+        {
+            Console.WriteLine("Tebrikler. Sepete eklendi : "+urun.Adi);
+
+        }
+
+
+        public void Ekle2(string urunAdi, string aciklama, double fiyat, int stokAdedi)
+        {
+            Console.WriteLine("Tebrikler. Sepete eklendi : " + urunAdi);
+        }
+    }
+}
