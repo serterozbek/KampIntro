25481aebd552c06c4b39a5ac416db11cf84de06f
serterozbek <serter.ozbek@hotmail.com>
2021-03-01 00:44:33 +0300

    Interface //23Ocak

---
+++ OOP3/KonutKrediManager.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP3
+{
+    class KonutKrediManager : IKrediManager
+    {
+        public void Hesapla()
+        {
+            ///Kendine özel kodlar
+            ///
+            Console.WriteLine("Konut kredisi ödeme planı hesaplandı.");
+        }
+    }
+}
---
+++ OOP3/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace OOP3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IKrediManager ihtiyacKrediManager = new IhtiyacKrediManager();
+            IKrediManager konutKrediManager = new KonutKrediManager();
+            IKrediManager tasitKrediManager = new TasitKrediManager();
+
+            ILoggerService databaseLoggerService = new DatabaseLoggerService();
+            ILoggerService fileLoggerService = new FileLoggerService();
+            
+
+            BasvuruManager basvuruManager = new BasvuruManager();
+            basvuruManager.BasvuruYap(tasitKrediManager,databaseLoggerService);
+            basvuruManager.BasvuruYap(new EsnafKredisiManager(), new SmsLoggerService());
+
+            List<IKrediManager> krediler = new List<IKrediManager>() {ihtiyacKrediManager,tasitKrediManager};
+
+            //basvuruManager.KrediOnBilgilendirmesiYap(krediler);
+        }
+
+    }
+}
